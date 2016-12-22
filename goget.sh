462bde0a13141158129e339b4e5e279b38578d24
Rich <rich@successful.im>
2016-12-23 00:39:00 +0100

    auto

---
+++ goget.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+goapp get -v -u github.com/golangdaddy/girlfriend/common
+goapp get -v -u github.com/golangdaddy/girlfriend/vanilla
+goapp get -v -u github.com/golangdaddy/girlfriend/appengine
